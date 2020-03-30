8541a470f3096fdf71e32252f453e810a07ed23f
Federico <federico.gomez.sa@gmail.com>
2020-03-29 21:58:30 -0600

    add Product class

---
+++ src/main/java/com/mx/ontech/apirest/productsservice/rest/ProductREST.java
@@ -0,0 +1,10 @@
+package com.mx.ontech.apirest.productsservice.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class ProductREST {
+}
