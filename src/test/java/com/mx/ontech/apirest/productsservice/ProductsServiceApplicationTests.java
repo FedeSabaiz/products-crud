91aa7780998b5a8bc9fba69a060a7d994f6bfe38
Federico <federico.gomez.sa@gmail.com>
2020-03-29 20:24:28 -0600

    init commit service products crud

---
+++ src/test/java/com/mx/ontech/apirest/productsservice/ProductsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mx.ontech.apirest.productsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
