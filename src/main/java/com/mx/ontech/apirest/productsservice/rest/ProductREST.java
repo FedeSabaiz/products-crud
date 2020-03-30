37e4ac3d6763dff21ae6f93647441a415b127aa3
Federico <federico.gomez.sa@gmail.com>
2020-03-30 00:35:54 -0600

    add conection to db and create an entity

--- src/main/java/com/mx/ontech/apirest/productsservice/rest/ProductREST.java
+++ src/main/java/com/mx/ontech/apirest/productsservice/rest/ProductREST.java
@@ -1,10 +1,21 @@
 package com.mx.ontech.apirest.productsservice.rest;
 
+import com.mx.ontech.apirest.productsservice.entitys.Product;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mx.ontech.apirest.productsservice.entitys.Product;
+
+import java.util.List;
+
 @RestController
-@RequestMapping("/")
+@RequestMapping("products")
 public class ProductREST {
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getProduct() {
+        return null;
+    }
 }
